cc9715f2ac5757988b75ddd58227910461b7223d
Lyza-Jorella <[EMAIL]>
2023-01-27 01:14:19 +0800

    Insertion Sort(2)

---
+++ Merge Sort.py
@@ -0,0 +1,48 @@
+print()
+print("**********Programmed by:**********")
+print("**********Lyza Jorella R. Del Rosario**********")
+print()
+
+def mergeSort(array):
+    if len(array) > 1:
+
+        r = len(array)//2
+        L = array[:r]
+        M = array[r:]
+
+        mergeSort(L)
+        mergeSort(M)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(M):
+            array[k] = M[j]
+            j += 1
+            k += 1
+
+def printList(array):
+    for i in range(len(array)):
+        print(array[i], end=" ")
+    print()
+
+if __name__ == '__main__':
+    array = [39, 84, 5, 86, 85, 14, 35, 45, 27, 43]
+
+    mergeSort(array)
+
+    print("Sorted array is: ")
+    printList(array)
