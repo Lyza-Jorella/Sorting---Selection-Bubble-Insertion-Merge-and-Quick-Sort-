b58c408c88d6bee0ccd6a130a3bb652c8b3d082b
Lyza-Jorella <[EMAIL]>
2023-01-26 23:49:06 +0800

    Selection Sort

--- Sorting - (Selection,Bubble, Insertion, Merge, and Quick Sort).py
+++ Sorting - (Selection,Bubble, Insertion, Merge, and Quick Sort).py
@@ -3,3 +3,23 @@ print("**********Programmed by:**********")
 print("**********Lyza Jorella R. Del Rosario**********")
 print()
 
+def sort(nums):
+
+    for i in range (39):
+        minpos = i
+        for j in range(i,10):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+
+        print(nums)
+
+nums = [39, 84, 5, 86, 85, 14, 35, 45, 27, 43]
+sort(nums)
+
+print(nums)
+
+
